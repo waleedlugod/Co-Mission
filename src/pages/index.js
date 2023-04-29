27a0c51c7d6bfbb0f197aec45f9d8d31af89071e
Waleed Lugod <lugodwaleed@gmail.com>
2023-04-30 03:20:54 +0800

    removed redundancy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Nav from "co-mission/components/Nav"
-import Header from "co-mission/components/Header"
-
-const inter = Inter({ subsets: ["latin"] });
+import Nav from "co-mission/components/Nav";
+import Header from "co-mission/components/Header";
 
 export default function Home() {
     return (
@@ -16,8 +14,8 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <Nav/>
-                <Header/>
+                <Nav />
+                <Header />
             </main>
         </>
     );
