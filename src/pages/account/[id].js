33c5e5adfbffd52789865cd14f2837429032ae80
Waleed Lugod <lugodwaleed@gmail.com>
2023-04-30 03:08:24 +0800

    added account page

--- src/pages/account/[id].js
+++ src/pages/account/[id].js
@@ -1,10 +1,30 @@
+import Image from "next/image";
 import { accounts } from "co-mission/data/accountsdata";
+import styles from "co-mission/styles/Account.module.css";
 
 export default function Account({ accountData }) {
     return (
-        <>
-            <h1>{accountData.name}</h1>
-        </>
+        <div className={styles.main}>
+            <div>
+                <div className={styles.imageWrapper}>
+                    <Image className={styles.image} src={accountData.pfp} fill alt="" />
+                </div>
+                <h1>{accountData.id}</h1>
+                <h4>{accountData.name}</h4>
+            </div>
+            <div className={styles.infoWrapper}>
+                <div className={styles.info}>
+                    <h2>Basic Info</h2>
+                    <p>Age: {accountData.age}</p>
+                    <p>{accountData.description}</p>
+                </div>
+                <div className={styles.info}>
+                    <h2>Commission Me!</h2>
+                    <p>Rates: {accountData.rates}</p>
+                    <p>{accountData.contact}</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
