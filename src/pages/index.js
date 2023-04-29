c3219a953dcac8072d2dabb6c32c0fc927c2d4d9
Waleed Lugod <lugodwaleed@gmail.com>
2023-04-30 03:21:08 +0800

    removed useless code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import Nav from "co-mission/components/Nav";
 import Header from "co-mission/components/Header";
 
