de240c2c73f46efef663bb469139a37b8ee36526
Waleed Lugod <lugodwaleed@gmail.com>
2023-04-29 19:26:59 +0800

    stff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Card from "co-mission/components/Card";
 
 export default function Home() {
     return (
@@ -13,7 +10,11 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main></main>
+            <main>
+                {[...Array(20)].map((curr, idx) => (
+                    <Card key={idx} href="/sky.jpg" />
+                ))}
+            </main>
         </>
     );
 }
